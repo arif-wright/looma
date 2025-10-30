d43ecfc83b1327baf8f7d3d3dc2c9a9f3b5b6737
Arif Wright <arif.wright@gmail.com>
2025-10-30 16:58:58 -0500

    chore(routes): remove legacy app route duplicates

--- src/routes/app/creatures/[id]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  export let data: any;
-  import CreatureDetailModal from '$lib/app/creatures/CreatureDetailModal.svelte';
-  let open = true;
-</script>
-
-<div class="container py-6">
-  <CreatureDetailModal {open} creature={data.creature} on:close={() => history.back()} />
-</div>
--- src/routes/app/u/[handle]/p/[id]/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import FocusedPostPage from '../../FocusedPostPage.svelte';
-  export let data;
-</script>
-
-<FocusedPostPage {data} />
